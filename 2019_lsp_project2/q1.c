fba5890855b80c3d41876d031a68a6377a7add6a
ZeroRim <zerorimchoi@gmail.com>
2020-06-01 16:27:31 +0900

    first test commit

---
+++ 2019_lsp_project2/q1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+int main(int argc,char * argv[])
+{
+		
+	printf("Enter the number : ");
+	
+	int num;
+	scanf("%d", &num);
+	int even=0, odd=0;
+	
+	for(int i=1; i<=num; i++)
+	{ // Checking...
+		if(i % 2 == 0){
+			even+=i;
+		}
+		else{
+			odd+=i;
+		}
+	}
+	
+	printf("Sum of Even number : %d\n", even);
+	printf("Sum of Odd number : %d\n", odd);
+	
+	return 0;
+}
